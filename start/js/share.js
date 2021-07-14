9b8ab396477a13fbdb9023f323a307f70760a283
Siyoung <happycloudy0308@gmail.com>
2021-07-14 15:22:32 +0900

    공유 화면 바꾸기 전까지 완료

--- start/js/share.js
+++ start/js/share.js
@@ -1,15 +1,14 @@
-const url = 'https://type4.netlify.app';
-
 function kakaoShare(){
     Kakao.Link.sendDefault({
         objectType: 'feed',
         content: {
-        title: shareTitle,
-        description: shareDes,
-        imageUrl: shareImage,
+        title: '디저트 사진',
+        description: '아메리카노, 빵, 케익',
+        imageUrl:
+            'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
         link: {
-            mobileWebUrl: shareURL,
-            webUrl: shareURL,
+            mobileWebUrl: 'https://developers.kakao.com',
+            androidExecutionParams: 'test',
         },
         },
         social: {
@@ -19,10 +18,15 @@ function kakaoShare(){
         },
         buttons: [
         {
-            title: '결과 확인하기',
+            title: '웹으로 이동',
             link: {
-            mobileWebUrl: shareURL,
-            webURL: shareURL
+        mobileWebUrl: 'https://developers.kakao.com',
+            },
+        },
+        {
+            title: '앱으로 이동',
+            link: {
+            mobileWebUrl: 'https://developers.kakao.com',
             },
         },
         ]
